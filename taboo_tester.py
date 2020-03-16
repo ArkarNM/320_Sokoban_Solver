8998e9ca8e4b877c13f98a9b1d5c636f951e79f4
Jamie Martin <49264143+JamieKieranMartin@users.noreply.github.com>
2020-03-17 08:12:39 +1000

    init and taboo cell partial implementation

---
+++ taboo_tester.py
@@ -0,0 +1,24 @@
+from __future__ import print_function
+from __future__ import division
+
+
+from sokoban import Warehouse, find_2D_iterator
+from mySokobanSolver import taboo_cells
+
+def test_taboo_cells(n):
+    problem_file = "./warehouses/warehouse_%s.txt" % str(n)
+    wh = Warehouse()
+    wh.load_warehouse(problem_file)
+    answer = taboo_cells(wh)
+    print(set(find_2D_iterator(answer.split('\n'), "X")))
+    print(answer)
+
+if __name__ == "__main__":
+    print("enter 'quit' to exit\n")
+    c = input('Warehouse number: ')
+    while c != 'quit':
+        try:
+            test_taboo_cells(c)
+        except FileNotFoundError as e:
+            print("Warehouse %s does not exist\n" % str(c))
+        c = input('Warehouse number: ')
